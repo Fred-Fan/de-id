b64769e4508fa2a01fa0c055982356f42807a86f
Fred-Fan <xuancheng.fan@berkeley.edu>
2017-10-23 17:07:28 -0700

    code update

--- src/pip/setup.py
+++ src/pip/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='philter',    # This is the name of your PyPI-package.
-    version='0.1.3',  # Update the version number for new releases
+    version='0.1.5',  # Update the version number for new releases
     packages=['philter'],
     #include_package_data=True,
     package_data={'philter': ['whitelist.pkl'],
